d916fe0f0efb7fccf81745f35d6da732788ddf96
YHCM <a2646399721@163.com>
2026-03-31 22:57:59 +0800

    前端测试提交

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,10 @@
 <script setup>
+import MainView from '@/views/MainView.vue';
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 </script>
 
 <template>
+  <el-config-provider :locale="zhCn">
+    <MainView />
+  </el-config-provider>
 </template>
